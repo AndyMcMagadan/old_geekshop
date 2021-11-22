0fdcd9c348e10d4afdc67ce68f6f9ca69cd718aa
AndyMcMagadan <mcmagadan@mail.ru>
2021-11-22 16:38:34 +0300

    Checked admin.urls

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
 
     path('categories/create/', admin_views.category_create, name='category_create'),
     path('categories/', admin_views.categories, name='category_list'),
-    path('categories/update/<int:pk>', admin_views.category_update, name='category_update'),
-    path('categories/delete/<int:pk>', admin_views.category_delete, name='category_delete'),
+    path('categories/update/<int:pk>/', admin_views.category_update, name='category_update'),
+    path('categories/delete/<int:pk>/', admin_views.category_delete, name='category_delete'),
 
-    path('products/create/<int:pk>', admin_views.product_create, name='product_create'),
-    path('products/', admin_views.products, name='product_list'),
-    path('products/update/<int:pk>', admin_views.product_update, name='product_update'),
-    path('products/delete/<int:pk>', admin_views.product_delete, name='product_delete'),
-    path('products/detail/<int:pk>', admin_views.product_detail, name='product_detail'),
+    path('products/create/<int:pk>/', admin_views.product_create, name='product_create'),
+    path('products/<int:pk>/', admin_views.products, name='product_list'),
+    path('products/update/<int:pk>/', admin_views.product_update, name='product_update'),
+    path('products/delete/<int:pk>/', admin_views.product_delete, name='product_delete'),
+    path('products/detail/<int:pk>/', admin_views.product_detail, name='product_detail'),
 ]
